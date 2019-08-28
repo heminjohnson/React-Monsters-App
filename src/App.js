7ab2403c018b5c7ecdd67d264174e1284c76b44f
Hemin Johnson <achayan1992@gmail.com>
2019-08-28 12:43:46 +0200

    feat: add the dynamic contents

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
 class App extends Component {
   constructor () {
     super()
 
     this.state = {
-      string: 'Hello Hemin Johnson'
+      monsters: [
+        {
+          name: 'Frankenstein',
+          id: '1'
+        },
+        {
+          name: 'Dracula',
+          id: '2'
+        },
+        {
+          name: 'Zombie',
+          id: '3'
+        }
+      ]
     }
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            { this.state.string }
-          </p>
-          <button onClick={() => {this.setState({string: 'Hemin'})}}>Change Text</button>
-        </header>
+        {
+          this.state.monsters.map((monster) => (
+            <h1 key={monster.id}>{monster.name}</h1>
+          ))
+        }
       </div>
     )
   }
